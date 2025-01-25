d9a31aa5301318aeed4092501e734761c4843cd7
John Yung <johnnyyungy@gmail.com>
2025-01-25 20:06:19 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Use HashRouter
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
